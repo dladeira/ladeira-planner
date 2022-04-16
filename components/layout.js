a91e36bff322f3ec85410a50ece7f620850ed9e9
dladeira <dladeiratx@gmail.com>
2022-04-16 14:59:46 +0200

    Fixed overview styling

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ function Component({ children }) {
 
                 {/* Fonts */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
 
                 {/* Icons */}
